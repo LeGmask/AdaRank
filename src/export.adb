6a9053dc64c160cd27045d25dc00b1b310d89fec
Evann DREUMONT <53308142+LeGmask@users.noreply.github.com>
2024-01-09 19:50:00 +0100

    fix: avoid unnecessary space in header

--- src/export.adb
+++ src/export.adb
@@ -1,3 +1,6 @@
+with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
+with Ada.Float_Text_IO;   use Ada.Float_Text_IO;
+
 package body Export is
     procedure Export_Resultats
        (Pi   : in T_Matrice; Ordre : in Matrice_Ordre.T_Matrice;
@@ -8,20 +11,22 @@ package body Export is
 
     begin
         Create (Fichier_Pi, Out_File, Prefix & ".prw");
-        Put_Line
-           (Fichier_Pi,
-            Integer'Image (N) & " " & Float'Image (Alpha) & " " &
-            Integer'Image (K));
+        Put (Fichier_Pi, N, 0);
+        Put (Fichier_Pi, " ");
+        Put (File => Fichier_Pi, Item => Alpha, Fore => 0, Exp => 0);
+        Put (Fichier_Pi, " ");
+        Put (Fichier_Pi, K, 0);
+        New_Line (Fichier_Pi);
 
         for J in 1 .. N loop
-            Put_Pi (Fichier_Pi, Get(Pi, 1, J));
+            Put_Pi (Fichier_Pi, Get (Pi, 1, J));
             New_Line (Fichier_Pi);
         end loop;
         Close (Fichier_Pi);
 
         Create (Fichier_Ordre, Out_File, Prefix & ".pr");
         for J in 1 .. N loop
-            Put_Ordre (Fichier_Ordre, Matrice_Ordre.Get(Ordre, 1, J));
+            Put_Ordre (Fichier_Ordre, Matrice_Ordre.Get (Ordre, 1, J));
             New_Line (Fichier_Ordre);
         end loop;
 
--- src/export.ads
+++ src/export.ads
@@ -1,4 +1,4 @@
-with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Text_IO;         use Ada.Text_IO;
 with Matrice;
 
 generic
@@ -6,17 +6,20 @@ generic
     use Matrice_Pi;
     with package Matrice_Ordre is new Matrice (<>);
 
-package Export is 
+package Export is
     generic
-        with procedure Put_Pi (File: File_Type; Valeur : Matrice_Pi.T_Valeur);
+        with procedure Put_Pi (File : File_Type; Valeur : Matrice_Pi.T_Valeur);
         -- Affichage des T_Valeurs correspondant au type Matrice_Pi.T_Valeur dans le fichier File
         -- @param File : fichier dans lequel écrit la procédure
         -- @param Valeur : valeur à écrire dans le fichier
-        with procedure Put_Ordre (File: File_Type; Valeur : Matrice_Ordre.T_Valeur);
-        -- Affichage des T_Valeurs correspondant au type Matrice_Ordre.T_Valeur dans le fichier File
-        -- @param File : fichier dans lequel écrit la procédure
-        -- @param Valeur : valeur à écrire dans le fichier
-    procedure Export_Resultats (Pi: in T_Matrice; Ordre: in Matrice_Ordre.T_Matrice; N, K: in Integer; Alpha: in Float; Prefix:  in String);
+        with procedure Put_Ordre
+           (File : File_Type; Valeur : Matrice_Ordre.T_Valeur);
+    -- Affichage des T_Valeurs correspondant au type Matrice_Ordre.T_Valeur dans le fichier File
+    -- @param File : fichier dans lequel écrit la procédure
+    -- @param Valeur : valeur à écrire dans le fichier
+    procedure Export_Resultats
+       (Pi   : in T_Matrice; Ordre : in Matrice_Ordre.T_Matrice;
+        N, K : in Integer; Alpha : in Float; Prefix : in String);
     -- Exporte les résultats de l'algorithme PageRank (Pi, Ordre (et N, K et Alpha)) dans les fichiers .pr et .prw dans le dossier courant, avec le préfixe Prefix
     -- @param Pi : Vecteur ligne des poids (trié)
     -- @param Ordre : Vecteur ligne des noeuds correspondants aux poids de Pi
