a919adcd7aff65fea436eb302f91a515db0a4a27
Evann DREUMONT <53308142+LeGmask@users.noreply.github.com>
2024-01-16 18:58:51 +0100

    fix: avoid overwriting results

--- benchmark.sh
+++ benchmark.sh
@@ -8,31 +8,31 @@ ulimit -s unlimited
 echo -e "## Sujet.net benchmark" > benchmark.md
 echo -e "- **Plein**" >> benchmark.md
 echo -e "\`\`\`sh" >> benchmark.md
-(time make run ARGS="networks/sujet.net") >> benchmark.md 2>> benchmark.md
+(time make run ARGS="-R benchmark networks/sujet.net") >> benchmark.md 2>> benchmark.md
 echo -e "\`\`\`" >> benchmark.md
 echo -e "- **Creux**" >> benchmark.md
 echo -e "\`\`\`sh" >> benchmark.md
-(time make run ARGS="-C networks/sujet.net") >> benchmark.md 2>> benchmark.md
+(time make run ARGS="-R benchmark -C networks/sujet.net") >> benchmark.md 2>> benchmark.md
 echo -e "\`\`\`" >> benchmark.md
 
 echo -e "## worm.net benchmark" >> benchmark.md
 echo -e "- **Plein**" >> benchmark.md
 echo -e "\`\`\`sh" >> benchmark.md
-(time make run ARGS="networks/worm.net") >> benchmark.md 2>> benchmark.md
+(time make run ARGS="-R benchmark networks/worm.net") >> benchmark.md 2>> benchmark.md
 echo -e "\`\`\`" >> benchmark.md
 echo -e "- **Creux**" >> benchmark.md
 echo -e "\`\`\`sh" >> benchmark.md
-(time make run ARGS="-C networks/worm.net") >> benchmark.md 2>> benchmark.md
+(time make run ARGS="-R benchmark -C networks/worm.net") >> benchmark.md 2>> benchmark.md
 echo -e "\`\`\`" >> benchmark.md
 
 echo -e "## brainlinks.net benchmark" >> benchmark.md
 echo -e "- **Creux**" >> benchmark.md
 echo -e "\`\`\`sh" >> benchmark.md
-(time make run ARGS="-C networks/brainlinks.net") >> benchmark.md 2>> benchmark.md
+(time make run ARGS="-R benchmark -C networks/brainlinks.net") >> benchmark.md 2>> benchmark.md
 echo -e "\`\`\`" >> benchmark.md
 
 echo -e "## linux26.net benchmark" >> benchmark.md
 echo -e "- **Creux**" >> benchmark.md
 echo -e "\`\`\`sh" >> benchmark.md
-(time make run ARGS="-C networks/linux26.net") >> benchmark.md 2>> benchmark.md
+(time make run ARGS="-R benchmark -C networks/linux26.net") >> benchmark.md 2>> benchmark.md
 echo -e "\`\`\`" >> benchmark.md
