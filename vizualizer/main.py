4659906b8b5fe1bf8ef879e63a2804e8cb7288db
Evann DREUMONT <53308142+LeGmask@users.noreply.github.com>
2024-01-16 18:46:48 +0100

    fix: fix vizualizer

--- vizualizer/main.py
+++ vizualizer/main.py
@@ -4,6 +4,8 @@ from networkx.drawing.nx_pydot import write_dot
 import matplotlib.pyplot as plt
 import os
 
+nt = Network('100vh', '100%')
+
 G = nx.Graph()
 
 # run page_rank using cli
@@ -15,25 +17,25 @@ with open("output.pr") as noeuds:
         # skip first line of scores
         scores.readline()
 
-        for idx, (noeud, score) in enumerate(zip(noeuds, scores)):
-            G.add_node(idx, label=str(noeud), size=float(score)*100, color=float(score)*100)
+        for noeud, score in zip(noeuds, scores):
+            nt.add_node(int(noeud), label=noeud, size=float(score)*100)
 
 with open("networks/sujet.net") as f:
+    # skip first line
+    f.readline()
+
     for idx, line in enumerate(f):
         line = line.split()
-        if idx == 0:
-            for i in range(int(line[0])):
-                G.add_node(i)
-        else:
-            G.add_edge(int(line[0]), int(line[1]))
+        
+        nt.add_edge(int(line[0]), int(line[1]))
 
+# print(G.edges)
 
-
-node_weight = list(nx.get_node_attributes(G,'size').values())
+# node_weight = list(nx.get_node_attributes(G,'size').values())
 # print(node_weight)
-
-# nx.draw_networkx(G, cmap=plt.cm.plasma, node_color=node_weight, font_color='cyan', font_size=8, width=0.5, edge_color='grey')
+# pos = nx.shell_layout(G)
+# nx.draw(G, pos=pos, cmap=plt.cm.plasma, node_color=node_weight, font_color='cyan', font_size=8, width=0.5, edge_color='grey')
 # plt.show()
-nt = Network('100vh', '100%')
-nt.from_nx(G)
-nt.show('test.html', notebook=False)
+
+# nt.from_nx(G)
+nt.show('graph.html', notebook=False)
