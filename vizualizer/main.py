6c6244d0958dc1a3d05c556103fca736ba055124
Evann DREUMONT <53308142+LeGmask@users.noreply.github.com>
2024-01-16 19:23:16 +0100

    fix: vizualizer is now working on school computers

--- vizualizer/main.py
+++ vizualizer/main.py
@@ -1,12 +1,7 @@
 from pyvis.network import Network
-import networkx as nx
-from networkx.drawing.nx_pydot import write_dot
-import matplotlib.pyplot as plt
 import os
 
 nt = Network('100vh', '100%')
-
-G = nx.Graph()
 
 # run page_rank using cli
 
@@ -29,13 +24,4 @@ with open("networks/sujet.net") as f:
         
         nt.add_edge(int(line[0]), int(line[1]))
 
-# print(G.edges)
-
-# node_weight = list(nx.get_node_attributes(G,'size').values())
-# print(node_weight)
-# pos = nx.shell_layout(G)
-# nx.draw(G, pos=pos, cmap=plt.cm.plasma, node_color=node_weight, font_color='cyan', font_size=8, width=0.5, edge_color='grey')
-# plt.show()
-
-# nt.from_nx(G)
 nt.show('graph.html', notebook=False)
